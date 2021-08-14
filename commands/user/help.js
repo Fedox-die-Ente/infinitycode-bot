3630e4e51ade24134ba387a23ac376dd4aba7edb
Fedox2605 <mrminecrafttv19@gmail.com>
2021-08-14 19:25:13 +0200

    Der Help Command wurde überarbeitet.

--- commands/user/help.js
+++ commands/user/help.js
@@ -1,8 +1,55 @@
+const Discord = require('discord.js')
+const emoji = require('../../emoji.json')
+const embed = require('../../embed.json')
+
 module.exports = {
-    category: 'Test',
-    description: 'Test Command',
-    testOnly: true,
+    category: 'User',
+    aliases: 'h',
+    description: 'Hilfe Menü',
+    ownerOnly: false,
+    cooldown: '2s',
     callback: ({message}) => {
-        message.reply("Pong!")
+        const hilfe = new Discord.MessageEmbed()
+            .setAuthor(`${embed.name}`, `${embed.logo}`)
+            .setColor(embed.maincolor)
+            .setFooter(embed.footer)
+            .setDescription('» Der Prefix von Bot ist $')
+            .addFields({
+                    name: `${emoji.owner} Owner - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.staff} Staff - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.admin2} Administration - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.moderation} Moderation - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.economy} Economy - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.misc} Misc - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.fun} Fun - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.musik} Music - (0)`,
+                    value: `\`Keine Befehle erkannt\``
+                },
+                {
+                    name: `${emoji.user} User - (1)`,
+                    value: `\`help\``
+                })
+        message.channel.send(hilfe)
     }
 }
