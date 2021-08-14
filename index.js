b7f82ccb633b56d8d07f077bcd7a3bc3dbc3da10
Fedox2605 <mrminecrafttv19@gmail.com>
2021-08-14 11:10:13 +0200

    index.js wurde bearbeitet.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-test
+const DiscordJS = require('discord.js')
+const WOKCommands = require('wokcommands')
+
+const client = new DiscordJS.Client({
+    partials: ['MESSAGE', 'REACTION']
+})
+
+client.on('ready', () => {
+
+    new WOKCommands(client, {
+        commandsDir: 'commands',
+        featuresDir: 'events',
+        messagesPath: '',
+        showWarns: true,
+        del: -1,
+        defaultLangauge: "english",
+        ignoreBots: false,
+        dbOptions: {
+            keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        },
+        disabledDefaultCommands: [
+            // 'help',
+            // 'command',
+            // 'language',
+            // 'prefix',
+            // 'requiredrole'
+        ]
+    })
+        .setDefaultPrefix('$')
+        .setColor(0xff0000)
+        .setMongoPath('Your path here')
+})
+
+client.login("YOUR TOKEN HERE")
