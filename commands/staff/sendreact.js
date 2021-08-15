a1ce538966ad48635716c8979efba1b919e3e010
Fedox2605 <mrminecrafttv19@gmail.com>
2021-08-15 10:53:15 +0200

    Der SendReact Befehl wurde überarbeitet.

--- commands/staff/sendreact.js
+++ commands/staff/sendreact.js
@@ -10,25 +10,31 @@ module.exports = {
     description: 'Sende das Reaction Menü',
     ownerOnly: false,
     cooldown: '2s',
-    callback: ({message}) => {
-        const react = new Discord.MessageEmbed()
-            .setColor(embed.maincolor)
-            .setAuthor(`${embed.name}`, `${embed.logo}`)
-            .setDescription('Drücke auf dem jeweiligen Knopf um die Rolle zu bekommen.')
-            .setFooter(`${embed.footer}`)
-        const seecoding = new disbut.MessageButton()
-            .setStyle('green')
-            .setLabel('[+] Coding Channel')
-            .setID('coding+');
-        const notseecoding = new disbut.MessageButton()
-            .setStyle('red')
-            .setLabel('[-] Coding Channel')
-            .setID('coding-');
+    callback: ({message, args}) => {
+        if (message.channel.id === '875667747806257172') {
+            const react = new Discord.MessageEmbed()
+                .setColor(embed.maincolor)
+                .setAuthor(`${embed.name}`, `${embed.logo}`)
+                .setDescription('Hey, du möchtest Hilfe auf unseren Server bekommen?\nBefolge bitte diese 4 Regeln.\n\n**1** » Nutze [Sourcebin](https://sourceb.in/) um deinen Code / Error hochzuladen. (Oder eine andere Seite) \n**2** » Erkläre bitte den Fehler richtig und sag bitte was der Code machen soll.\n**3** » Nutze den richtigen Channel für deine Frage.\n**4** » Wenn du Hilfe bekommen hast und es geht mache bitte "$danke (Helfer)" um den jenigen zu danken.\n\nKlicke unten auf die Knöpfe um Zugang zu den Coding Kanälen zu bekommen.')
+                .setFooter(`${embed.footer}`)
+            const seecoding = new disbut.MessageButton()
+                .setStyle('green')
+                .setLabel('[+] Coding Channel')
+                .setID('coding+');
+            const notseecoding = new disbut.MessageButton()
+                .setStyle('red')
+                .setLabel('[-] Coding Channel')
+                .setID('coding-');
 
-        const addrow = new disbut.MessageActionRow()
-            .addComponents([seecoding, notseecoding]);
+            const addrow = new disbut.MessageActionRow()
+                .addComponents([seecoding, notseecoding]);
 
-        message.channel.send({components: addrow, embed: react})
+            message.channel.send({components: addrow, embed: react})
+        } else if (message.channel.id === '875667650678771762') {
+            message.reply('Falscher Channel')
+        }
+
+        //message.reply(message.channel.id)
     }
 }
 
