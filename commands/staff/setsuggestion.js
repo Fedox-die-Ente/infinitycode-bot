c154bfb786f643ed711598c01f7bd01d6433ab3e
Fedox2605 <mrminecrafttv19@gmail.com>
2021-08-17 19:58:54 +0200

    Der Setsuggestion Command wurde erstellt.

---
+++ commands/staff/setsuggestion.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js')
+const emoji = require('../../emoji.json')
+const embed = require('../../embed.json')
+const suggestionSchema = require('../../schemas/suggestions-schema')
+const {fetchSuggestionChannels} = require('../../events/suggestion')
+
+
+module.exports = {
+    category: 'User',
+    aliases: 'h',
+    description: 'Hilfe Menü',
+    ownerOnly: false,
+    cooldown: '2s',
+    callback: async ({message}) => {
+        const channel = message.mentions.channels.first() || message.channel
+
+        const {
+            guild: {id: guildId},
+        } = message
+        const {id: channelId} = channel
+
+        await suggestionSchema.findOneAndUpdate(
+            {
+                _id: guildId,
+            },
+            {
+                _id: guildId,
+                channelId,
+            },
+            {
+                upsert: true,
+            }
+        )
+        let done = new Discord.MessageEmbed()
+            .setAuthor(`${embed.name}`, `${embed.logo}`)
+            .setColor(`${embed.maincolor}`)
+            .setFooter(`${embed.footer}`)
+            .setDescription(`${emoji.done} | Der Suggestion Channel wurde zu ${channel} gesetzt.`)
+        message.reply(done)
+
+        fetchSuggestionChannels(guildId)
+    }
+}
+
