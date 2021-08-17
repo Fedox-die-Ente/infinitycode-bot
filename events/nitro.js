6e4fd514a7dda4e4b7bb4b1046a6b5a1fb44f676
Fedox2605 <mrminecrafttv19@gmail.com>
2021-08-17 19:59:10 +0200

    Das Nitro Event wurde bearbeitet.

--- events/nitro.js
+++ events/nitro.js
@@ -5,12 +5,13 @@ const emoji = require('../emoji.json')
 
 module.exports = (client, message) => {
     client.on('nitroBoost', (booster) => {
+        const channel = client.channels.cache.find(channel => channel.id === '875653006387998750')
         const nitro = new Discord.MessageEmbed()
             .setAuthor(`${embed.name}`, `${embed.logo}`)
             .setColor(`${embed.maincolor}`)
             .setFooter(`${embed.footer}`)
             .setDescription(`${emoji.done} | ${booster} hat unseren Server geboostet. <3`)
-        message.guild.channels.cache.get('875653006387998750').send(nitro)
+        channel.send(nitro)
     })
 }
 
